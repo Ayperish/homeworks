6cc760dfbd8cb7a9921eb1a5ab61eef08e43e82a
Ayperish <ayperisha.909@gmail.com>
2023-08-12 18:34:13 +0600

    lala

---
+++ DZ2/app.js
@@ -0,0 +1,119 @@
+
+
+    //DZ1
+
+var starBucks = {
+    hotDrinks: {
+        hotChocolate: {
+            price: 480,
+            description: 'hot chocolate with milk and whipped cream'
+        },
+        teavana: {
+            price: 385,
+            description: 'tasty ' +
+                'tea with vanilla syrup pumps'
+        }
+    },
+    coldDrinks: {
+        frozenMango: {
+            price: 300,
+            description: 'refresh lemonade'
+        },
+        pinkDrink: {
+            price: 500,
+            description: 'refresh lemonade with strawberry'
+        }
+    },
+    coffee: {
+        cappuccino: {
+            price: 485,
+            description: 'italian drink with milk'
+        },
+        latte: {
+            price: 485,
+            description: ' coffee drink with milk foam'
+        },
+        americano: {
+            price: 480,
+            description: 'coffee drink'
+        }
+    },
+}
+
+// 2
+
+// var array = ['Bishkek', 'LosAngeles', 'Chicago', 'NewYork', 'Houston', 'Dallas', 'Philadelphia', 'Seattle' ]
+// for (var i = 0; i <array.length ; i++) {
+//     if (array[i].length > 7) {
+//         console.log(`Города с символами больше 7 - ${array[i]}`)
+//     }
+// }
+
+
+// 3
+
+
+// var day = prompt( 'Введите день недели!')
+// switch (day) {
+//     case '1':
+//         alert('Понедельник')
+//         break
+//     case'2':
+//         alert('Вторник')
+//         break
+//     case('3'):
+//         alert('Среда')
+//         break
+//     case('4'):
+//         alert('Четверг')
+//         break
+//     case('5'):
+//         alert('Пятница')
+//         break
+//     case('6'):
+//         alert('Суббота')
+//         break
+//     case ('7'):
+//         alert('Воскресенье')
+//         break
+//     default:
+//         alert('Нет такого дня недели!')
+// }
+
+
+
+// var user = "Aiperi"
+// var users = ["erxddcrc","awzserzxdxd","tfvyuhbjhnhk","dcrtfvbgyunh","qa2wsedrfy87t686","qwerty75755565756575"]
+//
+//
+// // for (var i = 0; i <=10; i++){
+// //     console.log(i)
+// //
+// // }
+// for (var i = 0; i<users.length; i++){
+//     if (users[i].length <= 6){
+//         console.log(users[i])
+//     }
+//     // console.log(users[i])
+// }
+
+
+
+
+
+
+
+
+//////////////////////////////////////
+
+
+
+
+
+var names = ['Aigerim', 'Hadija', 'Meerim', 'Askar', 'Rustam', 'Nurbek']
+for (var i = 0; i < names.length; i++){
+    if(names[i].length >= 5){
+        console.log(names[i
+            ])
+    }
+}
