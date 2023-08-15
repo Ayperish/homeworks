e0eb22eb49b2a38f117361d8b2d33057cfdc9239
Ayperish <ayperisha.909@gmail.com>
2023-08-15 20:37:48 +0600

    lala

---
+++ DZ3_Aiperi_29_03/app.js
@@ -0,0 +1,36 @@
+// var arrNums = [123, 234, 256, 345, 456, 567, 543, 67, 55,23, 28, 50]
+// var newArray = []
+// for(var i = 0; i <arrNums.length; i++){
+//     var num = arrNums[i].toString()
+//     if(num[0]=== '2' || num[0] === '5') {
+//         newArray.push(num)
+//     }
+// }
+//     console.log(newArray)
+//
+//
+
+
+
+// function getDataType(item){
+//     console.log(typeof item)
+// }
+// getDataType('Hello world!')
+// getDataType(58)
+// getDataType(false)
+
+
+
+
+// var tags = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1', 'p', 'h1', 'ol', 'br'];
+// var result = {};
+//
+// for (var tag of tags) {
+//     if (result[tag]) {
+//         result[tag] += 1;
+//     } else {
+// //         result[tag] = 1;
+//     }
+// }
+//
+// console.log(result);
